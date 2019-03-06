0bb053e10b5b6b8e97d496ab47249ca9bfc7a570
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2019-03-06 15:41:55 -0700

    Class practice

--- es6-review-practice/index.js
+++ es6-review-practice/index.js
@@ -1,11 +1,47 @@
 class Car {
-    constructor(){
-        this.brand = "";
-        this.model = "";
-        this.year = 0;
+    constructor(brand, model, year){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    displayCarInfo () {
+        console.log(this.brand, this.model, this.year)
     }
 }
 
-let car1 = new Car();
+let car1 = new Car("Ford", "Focus", 2019);
+let car2 = new Car("Toyota", "Corolla", 2007)
 
-console.log(car1)
+car1.displayCarInfo();
+car2.displayCarInfo();
+
+class FantasticBeast {
+    constructor(name = "", weight = "", color = "", powerLevel = 0){
+        this.name = name;
+        this.weight = weight;
+        this.color = color;
+        this.powerLevel = powerLevel;
+    }
+}
+
+let buckbeak = new FantasticBeast("Buckbeak", "1000 lb", "gray", 0);
+let blank = new FantasticBeast();
+
+console.log(buckbeak)
+console.log(blank)
+
+
+//Creating an extention of a class
+class Dragon extends FantasticBeast {
+    constructor(name, weight, color, powerLevel, breathWeapons, wingSpan){
+        super(name, weight, color, powerLevel);
+        this.breathWeapons = breathWeapons;
+        this.wingSpan = wingSpan;
+    }
+}
+
+let anglicon = new Dragon("Anglicon", "10,000 lb", "Black", 50000, ["Fire", "Noxious Gas", "Acid"], 20000)
+
+console.log(anglicon)
+
