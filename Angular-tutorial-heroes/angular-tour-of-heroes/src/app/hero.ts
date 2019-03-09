e34a52983fc84640b7571442d65bb550c389b737
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2019-03-09 14:10:11 -0700

    hero-detail has hero prop passed to it

--- Angular-tutorial-heroes/angular-tour-of-heroes/src/app/hero.ts
+++ Angular-tutorial-heroes/angular-tour-of-heroes/src/app/hero.ts
@@ -1,4 +1,6 @@
 export class Hero {
-    id: number;
-    name: string;
+    //Note that all we're really doing here is creating a "type"
+    //We're saying that a Hero object should have an id that's a number and a name that's a string.
+    id : number;
+    name : string;
 }
