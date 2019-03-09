7a2b5afb4a401353f6ba19c62f3ed3f40852b0cb
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2019-03-08 17:00:53 -0700

    Basic hero list and selected hero functionality set up

--- Angular-tutorial-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angular-tutorial-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero'
+import {HEROES} from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +8,14 @@ import {Hero} from '../hero'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Wonder Woman'
+  heroes = HEROES;
+  //Here you're declaring the variable, but not assigning a value to it
+  //Note that the syntax of including a colon and then Hero is a feature of TypeScript. You're saying that whatever value is eventually assigned to selectedHero MUST be an instance of the Hero class.
+  selectedHero: Hero;
+
+  //Here we're creating our function that will be accessed and used in our <li> click event listener
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
   constructor() {
---
+++ Angular-tutorial-heroes/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr. Strange' },
+    { id: 12, name: 'Thor' },
+    { id: 13, name: 'Natasha Rominov' },
+    { id: 14, name: 'Bruce Banner' },
+    { id: 15, name: 'Iron Man' },
+    { id: 16, name: 'Hawkeye' },
+    { id: 17, name: 'Scarlet Witch' },
+    { id: 18, name: 'Vision' },
+    { id: 19, name: 'Captain America' },
+    { id: 20, name: 'Wonder Woman' }
+]
